d4669b49d2f4682995a350057d1196ab4b01631c
Julien LUCIANI <korstg@gmail.com>
2019-02-11 15:41:37 +0100

    stupid coaching done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'ask' => 'questions#ask', as: :ask
+  get 'answer' => 'questions#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
